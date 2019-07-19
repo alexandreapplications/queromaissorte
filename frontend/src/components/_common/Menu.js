00786eeb63e08aee8ee682d6d374ab770e14f7f1
Alexandre Sampaio <at.sampaio@gmail.com>
2019-07-19 17:05:42 -0300

    Formatação da tela

--- frontend/src/components/_common/Menu.js
+++ frontend/src/components/_common/Menu.js
@@ -1,16 +1,10 @@
 import React from "react";
-
-import clsx from "clsx";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
 import List from "@material-ui/core/List";
-import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import ListItem from "@material-ui/core/ListItem";
@@ -18,12 +12,6 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import InboxIcon from "@material-ui/icons/MoveToInbox";
 import MailIcon from "@material-ui/icons/Mail";
-
-import { Route, Switch } from "react-router-dom";
-import HomePage from "./Home/HomePage";
-import AboutPage from "./About/AboutPage";
-import PageNotFound from "./PageNotFound";
-import LotofacilPage from "./Loteria/Lotofacil/LotofacilPage";
 
 const drawerWidth = 240;
 
@@ -82,56 +70,20 @@ const useStyles = makeStyles(theme => ({
     marginLeft: 0
   }
 }));
-function App() {
+
+export default function Menu() {
   const classes = useStyles();
   const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
-
-  function handleDrawerOpen() {
-    setOpen(true);
-  }
+  const [open, setOpen] = React.useState(true);
 
   function handleDrawerClose() {
     setOpen(false);
   }
 
   return (
-    // <div className={classes.root}>
-    //   <CssBaseline />
-    //   <Header />
-    //   <Menu />
-    //   <main>
-    //     <Switch>
-    //       <Route exact path="/" component={HomePage} />
-    //       <Route path="/about" component={AboutPage} />
-    //       <Route path="/lotofacil" component={LotofacilPage} />
-    //       <Route component={PageNotFound} />
-    //     </Switch>
-    //   </main>
-    // </div>
-    <div className={classes.root}>
+    <>
       <CssBaseline />
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open
-        })}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="Open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, open && classes.hide)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            Persistent drawer
-          </Typography>
-        </Toolbar>
-      </AppBar>
+
       <Drawer
         className={classes.drawer}
         variant="persistent"
@@ -173,21 +125,6 @@ function App() {
           ))}
         </List>
       </Drawer>
-      <main
-        className={clsx(classes.content, {
-          [classes.contentShift]: open
-        })}
-      >
-        <div className={classes.drawerHeader} />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/about" component={AboutPage} />
-          <Route path="/lotofacil" component={LotofacilPage} />
-          <Route component={PageNotFound} />
-        </Switch>
-      </main>
-    </div>
+    </>
   );
 }
-
-export default App;
